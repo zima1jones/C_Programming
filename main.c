bbf6be44588b1703998cd4a82f0328258e76bc47
Jordan Jones <jordan.jones@mintellity.com>
2024-12-25 21:15:18 -0500

    exercise 6 on the 6th chapter working with conditionals finally got this exercise done!

--- main.c
+++ main.c
@@ -14,26 +14,61 @@ void findDistance() {
 }
 
 void findGrades () {
-    grade = 90;
+    grade = 99;
 
     if (grade <= 60) {
-        printf("the grade is %d and you have failed!", grade);
+        printf("the grade is %d\n and you have an F!", grade);
     }
+
+
 //you are on exercise 6-3 find a way to get the dashes and crosses placed here
+//these are for the grades with a D!
     if (grade >= 61 && grade <= 70) {
         if (grade <= 63) {
-            printf("the grade is %d - and you have failed!", grade);
+            printf("the grade is %d - and you have a D-!", grade);
         }
-        printf("the grade is %d and you have have a D!", grade);
+        else if (grade >= 68 && grade <= 70) {
+            printf("the grade is %d and you have have a D+!", grade);
+        }
+        else {
+            printf("the grade is %d - and you have a D!", grade);
+        }
     }
+//these are for grades with a C!
     if (grade >= 71 && grade <= 80) {
-        printf("the grade is %d and you have a C!", grade);
+        if (grade <= 73) {
+            printf("the grade is %d - and you have a C-!", grade);
+        }
+        else if (grade >= 78 && grade <= 80) {
+            printf("the grade is %d and you have have a C+!", grade);
+        }
+        else {
+            printf("the grade is %d - and you have a C!", grade);
+        }
+
     }
     if (grade >= 81 && grade <= 90) {
-        printf("the grade is %d and you have a B!", grade);
+        if (grade <= 83) {
+            printf("the grade is %d - and you have a B-!", grade);
+        }
+        else if (grade >= 88 && grade <= 90) {
+            printf("the grade is %d and you have have a B+!", grade);
+        }
+        else {
+            printf("the grade is %d - and you have a B!", grade);
+        }
+
     }
-    if(grade >= 91) {
-        printf("the grade is %d and you have a A!", grade);
+    if (grade >= 91) {
+        if (grade <= 93) {
+            printf("the grade is %d - and you have a A-!", grade);
+        }
+        else if (grade >= 98 && grade <= 100) {
+            printf("the grade is %d and you have have a A+!", grade);
+        }
+        else {
+            printf("the grade is %d - and you have a A!", grade);
+        }
     }
 
 
