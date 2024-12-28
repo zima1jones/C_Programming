c23450033574e3afb95f38dc54c7337be8568237
Jordan Jones <jordan.jones@mintellity.com>
2024-12-28 10:56:30 -0500

    first exercise done it's a bit wonky but it works as intended

--- Chapter_7_Exercises.c
+++ Chapter_7_Exercises.c
@@ -5,11 +5,11 @@
 
 /* This program is used to convert imperial units into metric units
 The user inputs and number and the imperial unit and the computer outputs the value
-as a metric value. If a symbol isn't recognized the computer reutrns it back to the user.
+as a metric value. If a symbol isn't recognized the computer returns it back to the user.
  */
 int user_units;
 int metric_units;
-int user_units_format;
+char user_units_format;
 
 
 
@@ -17,10 +17,24 @@ void unitsConverter () {
   printf("Enter number of units: ");
   scanf("%d %c", &user_units, &user_units_format);
 
-  //chatper 7 comeback to this need a way to parse for inches as well (or text input in general)
-  if (user_units > 0) {
+  //chapter 7 comeback to this need a way to parse for inches as well (or text input in general)
+  if (user_units_format == 'i') {
     metric_units = (user_units * 2.54);
     printf("The metric units are %d centimeters", metric_units);
+  }
+
+  else if (user_units_format == 'f') {
+    metric_units = (user_units * 0.3048);
+    printf("the metric units are %d meters", metric_units);
+  }
+
+  else if (user_units_format == 'g') {
+    metric_units = (user_units * 3.78541);
+    printf("the metric units are %d liters", metric_units);
+  }
+
+  else {
+    printf("Invalid input");
   }
 }
 
