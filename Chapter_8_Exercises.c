2cd501b21c69be204b14b636773ce08ee8613841
Jordan Jones <jordan.jones@mintellity.com>
2025-01-20 21:53:45 -0500

    chapter 8 the first assignment was to build an 8x8 checker board with each cell having 5x3 dimesions. Took some time but I am writing many for loops

--- Chapter_8_Exercises.c
+++ Chapter_8_Exercises.c
@@ -8,21 +8,57 @@
 #include <stdlib.h>
 int i = 0;
 int j = 0;
+int k = 0;
+int l = 0;
 void checkerBoard() {
- for (i = 0; i <= 5; ++i) {
-     if (i % 5 == 0) {
-         printf("+\n");
-         for (j = 0; j < 3; ++j) {
-             printf("|\n");
-         }
-     }
-     printf("-");
+    for (l = 0; l < 8; ++l) {
+
+            for (i = 0; i < 49; ++i) {
+                if (i % 6 == 0) {
+                    printf("+");
+                }
+
+                else if (i == 48) {
+                    printf("\n");
+                }
+                else {
+                    printf("-");
+                }
+
+            }
+
+
+            printf("\n");
+            for (k = 0; k < 3; ++k) {
+                for (i = 0; i < 49; ++i) {
+                    if (i % 6 == 0) {
+                        printf("|");
+                    }
+                    else {
+                        printf(" ");
+                    }
 
 
 
 
+                }
+                printf("\n");
+            }
 
- }
+    }
+    for (i = 0; i < 49; ++i) {
+        if (i % 6 == 0) {
+            printf("+");
+        }
+
+        else if (i == 48) {
+            printf("\n");
+        }
+        else {
+            printf("-");
+        }
+
+    }
 
 }
 
